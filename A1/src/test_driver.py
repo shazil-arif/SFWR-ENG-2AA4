587213c25a28fef6c9cb15f359eef5c248ba71c6
Shazil Arif <arifs2@mcmaster.ca>
2020-01-10 11:35:55 -0500

    added more unit tests

--- A1/src/test_driver.py
+++ A1/src/test_driver.py
@@ -3,15 +3,32 @@
 #  @brief ?
 #  @date ?
 from date_adt import DateT
-import pos_adt
+from pos_adt import GPost
 
 
 def compare(description, expected, actual):
-    print("Description: {description}".format(description=description))
-    print("Expected: {expected}".format(expected=expected))  
-    print("Actual:   {actual}".format(actual=actual))
-    if(expected == actual): print('Result: ' + '\x1b[6;30;42m' + 'Passed' + '\x1b[0m')
-    else: print('Result: ' + '\x1b[1;37;41m' + 'Failed' + '\x1b[0m')
+    print("Description: {description}\n".format(description=description))
+
+    #if expected value is instance of DateT or GPost class
+    if(isinstance(expected,DateT) or isinstance(expected,GPost)): 
+        expected_keys = expected.__dict__
+        actual_keys = actual.__dict__
+        print("Expected properties")
+        for i in expected_keys:
+            print("{key} : {value}".format(key=i,value=expected_keys[i]))  
+        print("\nActual properties")
+        for i in actual_keys:        
+            print("{key} : {value}".format(key=i,value=actual_keys[i]))  
+        if(expected.__dict__ == actual.__dict__):
+            print('\nResult: ' + '\x1b[6;30;42m' + 'Passed' + '\x1b[0m')
+        else:
+            print('\nResult: ' + '\x1b[1;37;41m' + 'Failed' + '\x1b[0m')
+
+    else: #comparing other types...string, int, float etc.
+        print("Expected: {expected}".format(expected=expected))  
+        print("Actual:   {actual}".format(actual=actual))
+        if(expected == actual): print('\nResult: ' + '\x1b[6;30;42m' + 'Passed' + '\x1b[0m')
+        else: print('\nResult: ' + '\x1b[1;37;41m' + 'Failed' + '\x1b[0m')
     print("\n------------------------------------------------\n")
 
 def main():
@@ -30,6 +47,7 @@ def main():
     compare("testing getter method for month",test.month(),1)
     compare("testing getter method for year",test.year(),2020)
 
+
     #test next function
     #ideally for a functions like this the number of tests to run should be equal to or greater than the number of execution paths
     #there are 6 cases
@@ -40,7 +58,14 @@ def main():
     # v) Transition into the next month when the current month is february and it a leap year
     # vi) Transition into the next month when current month is february and it is not a leap year
 
-    compare("should return January 2nd 2020",test.next())
+    compare("testing next method, it should return January 2nd 2020 and pass",test.next(),DateT(2,1,2020))
+
+    compare("testing next method, it should return January 2nd 2020 but fail this time",test.next(),DateT(2,2,2020))
+
+    #test for transitioning into next month with current month having 31 days
+    test = DateT(31,1,2020)
+    compare("test for transitioning into next month with current month having 31 days. It should return february 1st 2020 and pass",test.next(),DateT(1,2,2020))
+
 
 
     

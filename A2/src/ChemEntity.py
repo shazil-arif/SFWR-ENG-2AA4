9a60816b37fce65f8f3c58aaba8e82669b1679db
Shazil Arif <shazilarif94@gmail.com>
2020-02-01 14:47:50 -0500

    updated MolecSet and ChemEntity

--- A2/src/ChemEntity.py
+++ A2/src/ChemEntity.py
@@ -2,7 +2,16 @@
 #  @author 
 import ChemTypes
 import ElmSet
-class ChemEntity():
+from abc import ABC,abstractmethod
+
+class ChemEntity(ABC):
+    @abstractmethod
+    def num_atoms(elm):
+        pass
+
+    @abstractmethod
+    def constit_elements():
+        pass
 
         
 
--- A2/src/MolecSet.py
+++ A2/src/MolecSet.py
@@ -10,8 +10,22 @@ from ElmSet import *
 
 class MolecSet(Set):
     ## @brief constructor method 
-    #  @param elm an element from the periodic table, from ChemTypes
     #  @param num an integer indicating the number of atoms of element elm
-    def __init__(self, elm, num):
+    #  @param elm an element from the periodic table, from ChemTypes
+    def __init__(self, num, elm):
         self.elm = elm
         self.num = num
+
+    ## @brief get the number of atoms of Element elm in the molecule
+    #  return state variable: num, integer indicating the number of atoms
+    def get_num(self):
+        return self.num
+
+    ## @brief get the Element in the molecule
+    #  return state variable: elm, indicating the element from ElementT
+    def get_elm(self):
+        return self.elm
+
+    
+
+    

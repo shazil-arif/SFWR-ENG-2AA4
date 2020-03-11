6b8264b575e630ab236f3e55748c192f2550df6d
Shazil Arif <arifs2@mcmaster.ca>
2020-03-10 20:27:52 -0400

    add more doxygen documentation

--- A3/src/LuT.java
+++ A3/src/LuT.java
@@ -1,6 +1,10 @@
-/**
-* @brief LuT contains an enumeration for different uses/types for Seq2D specifically for LanduseMapT
+/** 
+*  @file LuT.java
+*  Author: Shazil Arif
+*  Description: LuT contains an enumeration for different uses/types for Seq2D specifically for LanduseMapT
+*  Revised: March 10th 2020
 */
+
 public enum LuT{
     R,T,A,C;
 }
